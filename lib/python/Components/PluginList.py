261426df56b5c8628349d7d4adaf411c535e8a2e
rossi2000 <rossi_2000@hotmail.com>
2014-12-18 17:34:58 +0000

    [PluginList] tweak 1080 templates

--- lib/python/Components/PluginList.py
+++ lib/python/Components/PluginList.py
@@ -16,8 +16,8 @@ def PluginEntryComponent(plugin, width=440):
 	if screenwidth and screenwidth == 1920:
 		return [
 			plugin,
-			MultiContentEntryText(pos=(180, 1), size=(width-120, 35), font=2, text=plugin.name),
-			MultiContentEntryText(pos=(180, 38), size=(width-120, 25), font=3, text=plugin.description),
+			MultiContentEntryText(pos=(180, 0), size=(width-120, 35), font=2, text=plugin.name),
+			MultiContentEntryText(pos=(180, 35), size=(width-120, 30), font=3, text=plugin.description),
 			MultiContentEntryPixmapAlphaBlend(pos=(10, 7), size=(150, 60), png = png)
 		]
 	else:
@@ -33,7 +33,7 @@ def PluginCategoryComponent(name, png, width=440):
 	if screenwidth and screenwidth == 1920:
 		return [
 			name,
-			MultiContentEntryText(pos=(110, 15), size=(width-80, 35), font=2, text=name),
+			MultiContentEntryText(pos=(110, 10), size=(width-80, 40), font=2, text=name),
 			MultiContentEntryPixmapAlphaBlend(pos=(10, 0), size=(90, 75), png = png)
 		]
 	else:
@@ -59,8 +59,8 @@ def PluginDownloadComponent(plugin, name, version=None, width=440):
 	if screenwidth and screenwidth == 1920:
 		return [
 			plugin,
-			MultiContentEntryText(pos=(110, 1), size=(width-80, 35), font=2, text=name),
-			MultiContentEntryText(pos=(110, 38), size=(width-80, 25), font=3, text=plugin.description),
+			MultiContentEntryText(pos=(110, 0), size=(width-80, 35), font=2, text=name),
+			MultiContentEntryText(pos=(110, 35), size=(width-80, 30), font=3, text=plugin.description),
 			MultiContentEntryPixmapAlphaBlend(pos=(10, 0), size=(90, 75), png = png)
 		]
 	else:
