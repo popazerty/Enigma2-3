75909b944cd95e53812fb653d482e339f3a1674b
rossi2000 <rossi_2000@hotmail.com>
2015-01-09 19:49:07 +0000

    Revert "[ServiceList] tweak channelnumber for 1080"
    This reverts commit 67b6085089cef45bee5408898e41ad16680f85ef.

--- lib/python/Components/ServiceList.py
+++ lib/python/Components/ServiceList.py
@@ -322,12 +322,8 @@ class ServiceList(HTMLComponent, GUIComponent):
 			channelNumberWidth = 0
 			channelNumberSpace = 0
 		else:
-			if screenwidth and screenwidth == 1920:
-				channelNumberWidth = config.usage.alternative_number_mode.value and 50 or 80
-				channelNumberSpace = 15
-			else:
-				channelNumberWidth = config.usage.alternative_number_mode.value and 50 or 63
-				channelNumberSpace = 10
+			channelNumberWidth = config.usage.alternative_number_mode.value and 50 or 63
+			channelNumberSpace = 10
 
 		self.l.setElementPosition(self.l.celServiceNumber, eRect(0, 0, channelNumberWidth, self.ItemHeight))
 
